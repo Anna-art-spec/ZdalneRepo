b4c20b08c28da307bb8e797feffdba25bea62954
Anna <contantia.kot@gmail.com>
2024-01-20 12:54:15 +0100

    Iitial hello

---
+++ hello.py
@@ -0,0 +1,14 @@
+imie = "Ania"
+wiek = "23"
+waga = "55"
+data_urodzenia = "21.03.2000"
+
+znanie1 = f"nazywam się {imie} i mam {wiek} lat."
+zdanie2 = f"urodziłam się {data_urodzenia}. Ważę {waga} kg"
+print('+' + '-' *100 + '+')
+print('|{zdanie1:^100}|')
+print('|{zdanie2:^100}|')
+print('+' + '-' *100 + '+')
+
+bpm = 75
+print(f"")
