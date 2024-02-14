ffb98fbcbb7d5ab7af829b34f14311ca0654bbb6
Anna-art-spec <151976204+Anna-art-spec@users.noreply.github.com>
2024-02-14 11:22:28 +0100

    Update README.md.py

--- README.md.py
+++ README.md.py
@@ -4,36 +4,44 @@ import sys
 print("Będzie to projekt zaliczeniowy")
 file = open("help.txt", "r", encoding="utf-8")
 
-def zakoczenie_programu():
+def zakoczenie_programu():                                                                        #kończy program w wewnętrznej pętli
     haslo = input("naciśnij Enter aby zakończyć: ")
     if haslo == "":
         print("Koniec programu.")
-
     else:
         print(f"nieprawidłowy wybór klawisza")
-def wyswietl_ponumerowane_pliki():
+def wyswietl_ponumerowane_pliki():                                                                 #wyświetla pliki funkcją ls
     lista = os.listdir()
     ponumerowane_pliki = [f"[{numer}] {plik}" for numer, plik in enumerate(lista, start=1)]
 
-    # Wyświetl wszystkie ponumerowane pliki w jednej linii
+    # Wyświetla wszystkie ponumerowane pliki w jednej linii
     print(" ".join(ponumerowane_pliki))
 
-
-def utworz_nowy_plik():
+def utworz_nowy_plik():                                                                             #tworzy nowy plik
     while True:
         nazwa_pliku = input("Wpisz nazwę nowego pliku i naciśnij Enter: ")
 
-        if nazwa_pliku.strip():  # Sprawdź, czy nazwa pliku nie jest pusta
+        if nazwa_pliku.strip():  # Sprawdza, czy nazwa pliku nie jest pusta
             try:
                 with open(nazwa_pliku, 'w') as plik:
                     print(f"Plik o nazwie '{nazwa_pliku}' został utworzony.")
-                break  # Przerwij pętlę, jeśli plik został utworzony poprawnie
+                break  # Przerywa pętlę, jeśli plik został utworzony poprawnie
             except Exception as e:
                 print(f"Wystąpił błąd podczas tworzenia pliku: {e}")
         else:
             print("Nazwa pliku nie może być pusta. Spróbuj ponownie.")
 
-def utworz_katalog(nazwa_katalogu):
+def usun_plik(nazwa_pliku):                                                                        #usuwa plik
+    try:
+        os.remove(nazwa_pliku)
+        print(f"Plik '{nazwa_pliku}' został usunięty.")
+    except FileNotFoundError:
+        print(f"Plik o nazwie '{nazwa_pliku}' nie istnieje.")
+    except Exception as e:
+        print(f"Wystąpił błąd podczas usuwania pliku: {e}")
+
+
+def utworz_katalog(nazwa_katalogu):                                                                  #tworzy katalog
     try:
         os.mkdir(nazwa_katalogu)
         print(f"Katalog o nazwie '{nazwa_katalogu}' został utworzony.")
@@ -42,23 +50,31 @@ def utworz_katalog(nazwa_katalogu):
     except Exception as e:
         print(f"Wystąpił błąd podczas tworzenia katalogu: {e}")
 
+def usun_katalog(nazwa_katalogu):                                                                     #usuwa katalog
+    try:
+        os.rmdir(nazwa_katalogu)
+        print(f"Katalog '{nazwa_katalogu}' został usunięty.")
+    except FileNotFoundError:
+        print(f"Katalog o nazwie '{nazwa_katalogu}' nie istnieje.")
+    except OSError as e:
+        print(f"Wystąpił błąd podczas usuwania katalogu '{nazwa_katalogu}': {e}")
 
 
-def przejdz_do_katalogu():
+def przejdz_do_katalogu():                                                                             #przejście do katalogu
     while True:
         nazwa_katalogu = input("Wpisz nazwę katalogu, do którego chcesz przejść: ")
 
-        if nazwa_katalogu.strip():  # Sprawdź, czy nazwa katalogu nie jest pusta
-            if os.path.isdir(nazwa_katalogu):  # Sprawdź, czy podana nazwa istnieje jako katalog
-                os.chdir(nazwa_katalogu)  # Zmień bieżący katalog na podany
+        if nazwa_katalogu.strip():  # Sprawdza, czy nazwa katalogu nie jest pusta
+            if os.path.isdir(nazwa_katalogu):  # Sprawdza, czy podana nazwa istnieje jako katalog
+                os.chdir(nazwa_katalogu)  # Zmienia bieżący katalog na podany
                 print(f"Przejście do katalogu '{nazwa_katalogu}' zostało zakończone.")
-                break  # Przerwij pętlę, jeśli udało się przejść do katalogu
+                break  # Przerywa pętlę, jeśli udało się przejść do katalogu
             else:
                 print("Podany katalog nie istnieje. Spróbuj ponownie.")
         else:
             print("Nazwa katalogu nie może być pusta. Spróbuj ponownie.")
 
-def wyswietl_zawartosc_pliku(nazwa_pliku):
+def wyswietl_zawartosc_pliku(nazwa_pliku):                                                              #wyświetla zawartość pliku
     try:
         with open(nazwa_pliku, 'r', encoding='utf-8') as plik:
             zawartosc = plik.read()
@@ -69,7 +85,7 @@ def wyswietl_zawartosc_pliku(nazwa_pliku):
     except Exception as e:
         print(f"Wystąpił błąd podczas wczytywania pliku: {e}")
 
-def podsumowanie_ilosci_znakow(nazwa_pliku):
+def podsumowanie_ilosci_znakow(nazwa_pliku):                                                             #podaje ilość znaków
     try:
         with open(nazwa_pliku, 'r', encoding='utf-8') as plik:
             zawartosc = plik.read()
@@ -81,7 +97,7 @@ def podsumowanie_ilosci_znakow(nazwa_pliku):
     except Exception as e:
         print(f"Wystąpił błąd podczas wczytywania pliku: {e}")
 
-def zmien_nazwe_pliku(stara_nazwa, nowa_nazwa):
+def zmien_nazwe_pliku(stara_nazwa, nowa_nazwa):                                                          #zmienia nazwę pliku
     try:
         os.rename(stara_nazwa, nowa_nazwa)
         print(f"Nazwa pliku zmieniona z '{stara_nazwa}' na '{nowa_nazwa}'.")
@@ -90,7 +106,7 @@ def zmien_nazwe_pliku(stara_nazwa, nowa_nazwa):
     except Exception as e:
         print(f"Wystąpił błąd podczas zmiany nazwy pliku: {e}")
 
-def wyszukaj_w_pliku(nazwa_pliku, szukany_tekst):
+def wyszukaj_w_pliku(nazwa_pliku, szukany_tekst):                                                        #wyszukuje tekst w pliku
     try:
         with open(nazwa_pliku, 'r', encoding='utf-8') as plik:
             znaleziono = False
@@ -109,13 +125,13 @@ def wyszukaj_w_pliku(nazwa_pliku, szukany_tekst):
 while True:
     def main():
         while True:
-            # Pobierz bieżącą ścieżkę dostępu
+            # Pobiera bieżącą ścieżkę dostępu
             sciezka = os.getcwd()
 
-            # Wyświetl ścieżkę dostępu
+            # Wyświetla ścieżkę dostępu
             print(f"Aktualna ścieżka dostępu: {sciezka}")
 
-            # Wprowadź komendę od użytkownika
+            # Wprowadza komendę od użytkownika
             komenda = input("wpisz komendę z listy: ")
 
             if komenda == 'help':
@@ -137,6 +153,12 @@ while True:
                 zakoczenie_programu()
                 break
 
+            if komenda == "delcr":
+                nazwa_pliku = input("Podaj nazwę pliku do usunięcia: ")
+                usun_plik(nazwa_pliku)
+                zakoczenie_programu()
+                break
+
             if komenda == 'cd':
                 przejdz_do_katalogu()
                 zakoczenie_programu()
@@ -153,6 +175,11 @@ while True:
                 zakoczenie_programu()
                 break
 
+            if komenda == 'delmkdir':
+                nazwa_katalogu = input("Podaj nazwę katalogu do usunięcia: ")
+                usun_katalog(nazwa_katalogu)
+                zakoczenie_programu()
+                break
 
             if komenda == 'o':
                 nazwa_pliku = input("Wpisz nazwę pliku, którego zawartość chcesz wyświetlić i naciśnij Enter: ")
@@ -196,7 +223,7 @@ while True:
                 sys.exit()
 
 
-            # Wykonaj komendę
+            # Wykonuje komendę
             os.system(komenda)
 
     if __name__ == "__main__":
